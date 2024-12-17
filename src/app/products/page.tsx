bbf70b0c845efffefce7aa7963753dfae1b8c105
Mynul Islam <mynul.sakil@gmail.com>
2024-12-17 19:59:57 +0600

    Fix deployment error

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,8 @@
 import { Products } from "@/views/products";
+import { Suspense } from 'react';
 
 export default function ProductsRoot() {
-  return <Products />;
+  return  <Suspense fallback={<div>Loading products...</div>}>
+  <Products />
+</Suspense>;
 }
